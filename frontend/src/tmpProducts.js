0540c08577a8e8d436fe89cb47b8ca3e95ea6dda
Omer Kaplan <Omer.kaplan1998@cs.colman.ac.il>
2022-12-03 03:07:57 +0200

    first client

---
+++ frontend/src/tmpProducts.js
@@ -0,0 +1,44 @@
+export const products = [
+  {
+    title: "Bannanas",
+    price: "10₪",
+    firstPhotoUrl:
+      "http://whatsfresh.teamrocket.co.nz/images/produce/bananas-200x200.jpg",
+    description: "A yellow fruit which has lots of ashlegan and tastes good",
+  },
+  {
+    title: "Apple",
+    price: "8₪",
+    firstPhotoUrl:
+      "https://www.cupitfood.com/wp-content/uploads/2018/08/green-apple.jpg",
+    description: "An apple is an edible fruit produced by an apple tree.",
+  },
+  {
+    title: "Pancake",
+    price: "20₪",
+    firstPhotoUrl:
+      "https://cafedelites.com/wp-content/uploads/2016/03/3-Ingredient-Pancakes-37-1-200x200.jpg",
+    description:
+      "Hot-cake / flat cake, prepared from a starch-based batter that may contain eggs, milk and butter.",
+  },
+  {
+    title: "Bread",
+    price: "12₪",
+    firstPhotoUrl:
+      "https://www.jocooks.com/wp-content/uploads/2020/03/white-bread-1-200x200.jpg",
+    description:
+      "Bread is a staple food prepared from a dough of flour and water usually by baking.",
+  },
+  {
+    title: "Eggs",
+    price: "20₪",
+    firstPhotoUrl:
+      "https://ganico.co.za/wp-content/uploads/2020/11/Farm-Eggs-04-200x200.jpg",
+    description:
+      "Humans have eaten animal eggs for thousands of years. contains lots of protein.",
+  },
+];
+
+export default {
+  products,
+};
