b8ef649f602c3090f63892821d4abcf708137f86
Omer Kaplan <Omer.kaplan1998@cs.colman.ac.il>
2022-12-03 05:26:11 +0200

    Much more

--- frontend/src/components/app/App.js
+++ frontend/src/components/app/App.js
@@ -2,11 +2,12 @@ import React, { useState } from "react";
 import AppNavbar from "./navbar/AppNavbar";
 import Mainbody from "./mainbody/Mainbody";
 import UserCart from "./usercart/UserCart";
+import { products } from "../../tmpProducts";
 import "./App.css";
 
 function App() {
-  const [isCartDialogOpen, setIsCartDialogOpen] = useState(true);
-  const [userShoppingCart, setUserShoppingCart] = useState({});
+  const [isCartDialogOpen, setIsCartDialogOpen] = useState(false); // change to start false
+  const [userShoppingCart, setUserShoppingCart] = useState([]);
 
   const modalToggle = () => {
     setIsCartDialogOpen((prevIsCartDialogOpen) => !prevIsCartDialogOpen);
@@ -16,15 +17,50 @@ function App() {
     modalToggle();
   };
 
-  const openDialog = () => {
+  const onAdd = (product) => {
+    const exist = userShoppingCart.find((item) => item.id === product.id);
+    if (exist) {
+      const newUserShoppingCart = userShoppingCart.map((item) =>
+        item.id === product.id ? { ...exist, qty: exist.qty + 1 } : item
+      );
+      setUserShoppingCart(newUserShoppingCart);
+    } else {
+      const newUserShoppingCart = [...userShoppingCart, { ...product, qty: 1 }];
+      setUserShoppingCart(newUserShoppingCart);
+    }
     setIsCartDialogOpen(true);
+  };
+
+  const onRemove = (product) => {
+    const exist = userShoppingCart.find((item) => item.id === product.id);
+    if (exist.qty === 1) {
+      const newUserShoppingCart = userShoppingCart.filter(
+        (item) => item.id !== product.id
+      );
+      setUserShoppingCart(newUserShoppingCart);
+    } else {
+      const newUserShoppingCart = userShoppingCart.map((item) =>
+        item.id === product.id ? { ...exist, qty: exist.qty - 1 } : item
+      );
+      setUserShoppingCart(newUserShoppingCart);
+    }
   };
 
   return (
     <div className="App">
-      <AppNavbar />
-      <Mainbody onOpen={openDialog} />
-      {isCartDialogOpen && <UserCart onClose={closeDialog} />}
+      <AppNavbar
+        shoppingQty={userShoppingCart.length}
+        onCartClick={modalToggle}
+      />
+      <Mainbody onAdd={onAdd} products={products} />
+      {isCartDialogOpen && (
+        <UserCart
+          cart={userShoppingCart}
+          onClose={closeDialog}
+          onAdd={onAdd}
+          onRemove={onRemove}
+        />
+      )}
     </div>
   );
 }
--- frontend/src/components/app/usercart/UserCart.jsx
+++ frontend/src/components/app/usercart/UserCart.jsx
@@ -1,14 +1,86 @@
 import React from "react";
+import Container from "react-bootstrap/Container";
+import Button from "react-bootstrap/Button";
 import "./usercart.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function UserCart(props) {
-  const { onClose } = props;
+  const { cart, onClose, onAdd, onRemove } = props;
+  const cartPrice = cart.reduce((a, c) => a + c.qty * c.price, 0);
+  const israelTax = cartPrice * 0.17;
+  const cartSumPrice = cartPrice + israelTax;
+
   return (
-    <div id="user-cart">
-      <div id="cart-title">
-        Cart! <button id="close-cart" onClick={onClose}>X</button>
-      </div>
-    </div>
+    <aside className="block col-1" id="user-cart">
+      <Container>
+        <div className="row">
+          <h4>Cart Items</h4>
+        </div>
+        <button id="close-cart" onClick={onClose}>
+          X{" "}
+        </button>
+        <hr />
+        <div className="row">
+          <div className="row" id="wrapper">
+            {cart.map((item) => (
+              <div key={item.id} className="row">
+                <div className="col-1">{item.title}</div>
+                <div className="col-1">
+                  <Button
+                    variant="outline-primary"
+                    size="sm"
+                    onClick={() => onAdd(item)}
+                  >
+                    +
+                  </Button>{" "}
+                  <Button
+                    variant="outline-danger"
+                    size="sm"
+                    onClick={() => onRemove(item)}
+                  >
+                    -
+                  </Button>{" "}
+                </div>
+                <div className="col-1 text-right">
+                  {item.qty} X ${item.price.toFixed(2)}
+                </div>
+              </div>
+            ))}
+            {cart.length !== 0 && (
+              <>
+                <hr />
+                <div className="row">
+                  <span className="col-2">Items Price</span>
+                  <span className="col-1 text-right">
+                    ${cartPrice.toFixed(2)}
+                  </span>
+                </div>
+                <div className="row">
+                  <span className="col-2">Tax Price</span>
+                  <span className="col-1 text-right">
+                    ${israelTax.toFixed(2)}
+                  </span>
+                </div>
+                <div className="row">
+                  <span className="col-2">
+                    <strong>Total price</strong>
+                  </span>
+                  <span className="col-1 text-right">
+                    <strong>${cartSumPrice.toFixed(2)}</strong>
+                  </span>
+                </div>
+                <hr />
+              </>
+            )}
+          </div>
+          <div className="row">
+            <Button variant="outline-info" onClick={() => alert("Check")}>
+              Checkout
+            </Button>
+          </div>
+        </div>
+      </Container>
+    </aside>
   );
 }
 
