1f95b3db5af09fe8587a0908022406b8ec627a35
AfikNof95 <afiknof9@gmail.com>
2022-12-13 04:50:57 +0200

    fix

--- backend/models/product.model.js
+++ backend/models/product.model.js
@@ -5,7 +5,6 @@ const Product = new Schema({
   name: { type: String, required: true },
   description: { type: String, required: true },
   price: { type: Number, required: true },
-  stock: { type: Number, required: true },
   category: { type: Schema.Types.ObjectId, ref: "Category", required: true },
   firstPhotoUrl: String,
 });
